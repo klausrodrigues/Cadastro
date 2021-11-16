a16a362e33545bcefdf73c829b6448d8c912f37d
Klaus Weber Batista Rodrigues <klausrodrigues@sf.intra>
2021-11-16 01:13:00 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroUsuario/Migrations/20211115182255_CriacaoBD.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CadastroUsuario.Migrations
+{
+    public partial class CriacaoBD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Pessoaid = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<string>(nullable: true),
+                    CPFCNPJ = table.Column<string>(nullable: true),
+                    TipoPessoa = table.Column<string>(nullable: true),
+                    Endereco = table.Column<string>(nullable: true),
+                    Número = table.Column<string>(nullable: true),
+                    Complemento = table.Column<string>(nullable: true),
+                    Bairro = table.Column<string>(nullable: true),
+                    CEP = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    Estado = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Pessoaid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
---
+++ CadastroUsuario/Models/Pessoa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroUsuario.Models
+{
+    public class Pessoa
+    {
+        public int Pessoaid { get; set; }
+
+        public string Nome { get; set; }
+
+        [Display(Name = "Data de nascimento")]
+        public string DataNascimento { get; set; }
+               
+        [Display(Name = "CPF/CNPJ")]
+        public string CPFCNPJ { get; set; }
+
+        [Display(Name = "Física/Jurídica")]
+        public string TipoPessoa { get; set; }
+
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Número")]
+        public string Número { get; set; }
+
+        [Display(Name = "Complemento")]
+        public string Complemento { get; set; }
+
+        [Display(Name = "Bairro")]
+        public string Bairro { get; set; }
+
+        [Display(Name = "CEP")]
+        public string CEP { get; set; }
+
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
